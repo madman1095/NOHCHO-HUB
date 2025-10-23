4190f1baa40499f8639c7c409875e5c2d5c6df84
madman1095 <so2softplut@gmail.com>
2025-10-23 19:25:45 +0300

    Update Speed.lua

--- Speed.lua
+++ Speed.lua
@@ -1,3 +1,56 @@
-while game:GetService("RunService").RenderStepped:wait() do
-  workspace.Princessmaryam095.Humanoid.WalkSpeed = 40
-end
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shl...)
+
+local Window = OrionLib:MakeWindow({Name = "NOHCHO HUB", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+
+
+-- player tab
+
+local PlayerTab = Window:MakeTab({
+ Name = "Player",
+ Icon = "rbxassetid://4483345998",
+ PremiumOnly = false
+})
+
+local Section = PlayerTab:AddSection({
+ Name = "Movement"
+})
+
+PlayerTab:AddSlider({
+ Name = "Walkspeed",
+ Min = 16,
+ Max = 500,
+ Default = 16,
+ Color = Color3.fromRGB(255,255,255),
+ Increment = 1,
+ ValueName = "WS",
+ Callback = function(Value)
+  game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = Value
+ end    
+})
+
+PlayerTab:AddSlider({
+ Name = "Jump Height",
+ Min = 16,
+ Max = 500,
+ Default = 5,
+ Color = Color3.fromRGB(255,255,255),
+ Increment = 1,
+ ValueName = "Height",
+ Callback = function(Value)
+  game.Players.LocalPlayer.Character.Humanoid.JumpPower = Value
+ end    
+})
+
+
+--other tab
+
+local OtherTab = Window:MakeTab({
+ Name = "Other",
+ Icon = "rbxassetid://4483345998",
+ PremiumOnly = false
+})
+
+local Section = OtherTab:AddSection({
+ Name = "Other"
+  end
+})
